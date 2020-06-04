3371b68cae21f92f3a193ac215f559dbb1d20743
rsherlocks <hasan15-7482@diu.edu.bd>
2020-06-04 14:38:20 +0600

    frist comment

---
+++ app/src/androidTest/java/com/example/rakib/currency_exchange/MainActivityTest.java
@@ -0,0 +1,14 @@
+package com.example.rakib.currency_exchange;
+
+import org.junit.Test;
+
+/**
+ * Created by RAKIB on 18-Oct-17.
+ */
+public class MainActivityTest {
+    @Test
+    public void onCreate() throws Exception {
+
+    }
+
+}
